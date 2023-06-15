85ff17a707a8b417b2a61e689e4f4fc1c2a41342
Naisalie <naisaliew@gmail.com>
2023-06-15 06:13:44 -0400

    Add clarifying comments

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const container = document.querySelector(".container");
+//Function to create a singular square
 const makeSquare = () => {
   let newSquare = document.createElement("div");
   newSquare.classList.add("grid-square");
   container.appendChild(newSquare);
 };
 
+//Function to create 256 squares for 16x16 grid
 const makeGrid = () => {
   for (let i = 0; i < 256; i++) {
     makeSquare();
   }
 };
 
+//Load squares on window load
 window.addEventListener("load", makeGrid());
