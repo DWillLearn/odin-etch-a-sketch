20a0db691874afb761f4f07cbaa23c9358284870
Naisalie <naisaliew@gmail.com>
2023-06-22 02:41:45 -0400

    Reorder functions

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-const container = document.querySelector(".container");
-let gridBox = document.querySelector(".grid-box");
+let body = document.querySelector("body");
 
-//Function to create a singular square and add it to grid box
-const makeSquare = () => {
-  let newSquare = document.createElement("div");
-  newSquare.classList.add("grid-square");
-  gridBox.appendChild(newSquare);
+//Function to append grid to page
+const addGrid = () => {
+  let gridBox = document.createElement("div");
+  gridBox.classList.add("grid-box");
+  body.appendChild(gridBox);
+  gridBox = document.querySelector(".grid-box");
+  addGridBtn(gridBox);
+};
+
+//Add button to top of the screen that generates different grid size
+const addGridBtn = (container) => {
+  //Create button
+  let gridBtn = document.createElement("button");
+  gridBtn.classList.add("generate-grid");
+  gridBtn.textContent = "New Grid";
+  //Append button to top of screen
+  body.appendChild(gridBtn);
+  //Add event listener to button
+  gridBtn.addEventListener("click", () => {
+    let gridSize = prompt("Enter number of squares per side");
+    if (gridSize < 100) {
+      //Remove grid and replace with new one
+      container.innerHTML = "";
+      makeGrid(gridSize);
+    } else {
+      //Change to something on screen later
+      console.log("Error! Number too big");
+    }
+  });
 };
 
 //Function to create multiple squares in grid box
@@ -15,7 +38,13 @@ const makeGrid = (num) => {
     makeSquare();
   }
   hoverSquare();
-  newGrid();
+};
+//Function to create a singular square and add it to grid box
+const makeSquare = () => {
+  let gridBox = document.querySelector(".grid-box");
+  let newSquare = document.createElement("div");
+  newSquare.classList.add("grid-square");
+  gridBox.appendChild(newSquare);
 };
 
 //Set up hover effect so div changes color when passed over and stays color
@@ -28,29 +57,8 @@ const hoverSquare = () => {
   });
 };
 
-//Add button to top of the screen that generates different grid size
-const newGrid = () => {
-  //Create button
-  let gridBtn = document.createElement("button");
-  gridBtn.classList.add("generate-grid");
-  gridBtn.textContent = "New Grid";
-  //Append button to top of screen
-  container.appendChild(gridBtn);
-  //Add event listener to button
-  gridBtn.addEventListener("click", () => {
-    let gridSize = prompt("Enter number of squares per side");
-    if (gridSize < 100) {
-      //Remove grid and replace with new one
-      gridBox.innerHTML = "";
-      makeGrid(gridSize);
-    } else {
-      //Change to something on screen later
-      console.log("Error! Number too big");
-    }
-  });
-};
-
 //Load squares on window load
 window.addEventListener("load", () => {
+  addGrid();
   makeGrid(16);
 });
